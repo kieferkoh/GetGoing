861c888f8e06e16c8ebc046195dba3325632df23
kieferkoh <kieferkoh@gmail.com>
2024-03-28 19:54:55 +0800

    GetGoing App

--- app/src/main/java/com/example/getgoing/DatabaseManager.kt
+++ app/src/main/java/com/example/getgoing/DatabaseManager.kt
@@ -1,10 +1,50 @@
 package com.example.getgoing
 
+import User
 import com.google.firebase.database.DatabaseException
 import com.google.firebase.database.DatabaseReference
 import kotlinx.coroutines.tasks.await
 
 object DatabaseManager {
+
+//    suspend fun fetchUserFromFirebase(dbRef: DatabaseReference): User? {
+//        return try {
+//            val dataSnapshot = dbRef.get().await()
+//            val userData = dataSnapshot.value as? HashMap<*, *>
+//            if (userData != null && userData.isNotEmpty()) {
+//                val name = userData["name"] as? String?
+//                val phone = userData["phone"] as? String?
+//                val password = userData["password"] as? String?
+//                val groups = userData["groups"] as? HashMap<*, *>?
+//                val friends = userData["friends"] as? HashMap<*, *>?
+//
+//                User(name, phone, password, groups, friends)
+//            } else {
+//                null
+//            }
+//        } catch (e: Exception) {
+//            // Handle errors
+//            null
+//        }
+//    }
+suspend fun fetchUserInfoFromFirebase(dbRef: DatabaseReference): User? {
+    return try {
+        val dataSnapshot = dbRef.get().await()
+        val userData = dataSnapshot.value as? HashMap<*, *>
+
+        if (userData != null && userData.isNotEmpty()) {
+            val name = userData["name"] as? String?
+            val phone = userData["phone"] as? String?
+            val password = userData["password"] as? String?
+            User(name!!, phone!!, password!!)
+        } else {
+            null
+        }
+    } catch (e: Exception) {
+        // Handle errors
+        null
+    }
+}
 
     suspend fun <T : Any> fetchDataListFromFirebase(
         dbRef: DatabaseReference,
